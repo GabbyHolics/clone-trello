3c8deeaf251f85caadcf1e7ef06b4f5cd0781f23
GabbyHolics <karenthgabriela@gmail.com>
2022-01-06 15:38:08 -0300

    install vue.js

---
+++ clone-trello/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <router-view/>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader'
+import AppFooter from '@/components/AppFooter'
+
+export default {
+  name: 'App',
+  components: { AppHeader, AppFooter },
+}
+</script>
+
+<style>
+#app {
+  box-sizing:border-box;
+  color: rgb(189, 189, 189);
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin: 0 auto 
+}
+</style>
